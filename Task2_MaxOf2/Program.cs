7d6b489a1077a39df8ad531d50051348ebd99b5a
Lyubov <lyubasher@mail.ru>
2022-12-05 00:32:56 +0200

    task 2

---
+++ Task2_MaxOf2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+Console.WriteLine("Enter the number A:");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number B:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+
+if (A > B)
+{
+    Console.WriteLine("max =" +A);
+}
+else
+{
+    Console.WriteLine("max =" +B);
+}
