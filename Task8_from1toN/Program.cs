7cff9225db18d29af1b2b881fd5278c971173e2f
Lyubov <lyubasher@mail.ru>
2022-12-05 01:47:52 +0200

    task8

---
+++ Task8_from1toN/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Enter the number > 0 :");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = 2;
+
+Console.WriteLine("List of required numbers:");
+
+while ( M <=N )
+{
+    Console.WriteLine(M);
+    M+=2;
+}
