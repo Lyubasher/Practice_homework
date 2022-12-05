a17f3fecc9e588979a5bae2d13b110914ef455fa
Lyubov <lyubasher@mail.ru>
2022-12-06 01:04:22 +0200

    task13 update

---
+++ Task13_thirdNum/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter number > 0:");
+
+string number = Console.ReadLine();
+if (number.Length<=2)
+{
+    Console.WriteLine("the thitd digit is missing.");
+}
+else
+{
+    Console.WriteLine("the third digit is " +number[2]);
+}
+
