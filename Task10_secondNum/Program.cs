185c5b368c0c03265172ef7877b7da06100f5273
Lyubov <lyubasher@mail.ru>
2022-12-06 02:31:44 +0200

    task 10 update

--- Task10_secondNum/Program.cs
+++ Task10_secondNum/Program.cs
@@ -5,12 +5,11 @@ Console.WriteLine("Enter three-digit number:");
 
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > -999 && number < -99)
+if (number > -1000 && number < -99)
 {
     number = - number;
     Console.WriteLine("the second digit:" +number % 100 / 10);
 }
-
 else
 {
     if (number > 99 && number < 1000)
