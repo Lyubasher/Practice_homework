16f5347d670c080bd75f35a2e72d4b5dff798bc5
Lyubov <lyubasher@mail.ru>
2022-12-06 18:25:56 +0200

    task13 update

--- Task13_thirdNum/Program.cs
+++ Task13_thirdNum/Program.cs
@@ -1,15 +1,31 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру 
 // заданного числа или сообщает, что третьей цифры нет.
 
-Console.WriteLine("Enter number > 0:");
-
-string number = Console.ReadLine();
-if (number.Length<=2)
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
 {
-    Console.WriteLine("the thitd digit is missing.");
+    string str = Convert.ToString(number);
+    if (str.Length<=3)
+    {
+       Console.WriteLine("the thitd digit is missing."); 
+    }
+    else
+    {
+        Console.WriteLine("the third digit is " +str[3]);
+    }
 }
-else
+
+if (number > 0)
 {
-    Console.WriteLine("the third digit is " +number[2]);
+    string str = Convert.ToString(number);
+    if (str.Length<=2)
+    {
+        Console.WriteLine("the thitd digit is missing.");
+    }
+    else
+    {
+        Console.WriteLine("the third digit is " +str[2]);
+    }
 }
 
